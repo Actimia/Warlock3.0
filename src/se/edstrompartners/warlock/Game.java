ceb9ea9a6a6bf257b5d13816946b293ef0548379
Actimia <actimia1@gmail.com>
2014-05-20 18:33:12 +0200

    Libs added.

--- src/se/edstrompartners/warlock/Game.java
+++ src/se/edstrompartners/warlock/Game.java
@@ -1,7 +1,24 @@
 package se.edstrompartners.warlock;
 
-public class Game {
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Game extends StateBasedGame {
+
+    public Game() {
+        super("TDF Warlock 3.0");
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, Warlock!");
+
+        new Game();
+    }
+
+    @Override
+    public void initStatesList(GameContainer arg0) throws SlickException {
+        // TODO Auto-generated method stub
+
     }
 }
